e51a6e2150f60065d05a837e5d78ef17678dd2c4
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-09-24 23:48:50 +0100

    Delete 333-img-choser directory

--- 333-img-choser/src/config.js
+++
@@ -1,4 +0,0 @@
-export const NFTCONTRACT = "0x7E82F54578f03ad922F2bA61674C46b9dBdCE61d";
-export const IMAGECONTRACT = "0x53b0f31db043d6ebe70ff49505a42e5d0e8f2a51";
-export const moralisapi = "https://deep-index.moralis.io/api/v2/";
-export const moralisapikey = "2VBV4vaCLiuGu6Vu7epXKlFItGe3jSPON8WV4CrXKYaNBEazEUrf1xwHxbrIo1oM";
